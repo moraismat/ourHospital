54aa95dac6c6d4336c174bcd75cbf05092401124
Matheus Morais <morais629@gmail.com>
2023-06-12 18:06:10 -0300

    OpenPDF

--- src/main/java/com/hospital/ourhospital/controller/EventoController.java
+++ src/main/java/com/hospital/ourhospital/controller/EventoController.java
@@ -1,9 +1,53 @@
 package com.hospital.ourhospital.controller;
 
+import com.lowagie.text.DocumentException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.*;
 
 @RestController
 @RequestMapping("/evento")
 public class EventoController {
+
+    private String parseThymeleafTemplate() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        Context context = new Context();
+        context.setVariable("to", "Baeldung");
+
+        return templateEngine.process("/templates/template", context);
+    }
+
+    public void generatePdfFromHtml(String html) throws IOException, DocumentException {
+        String outputFolder = "./pdf.pdf";
+        OutputStream outputStream = new FileOutputStream(outputFolder);
+
+        ITextRenderer renderer = new ITextRenderer();
+        renderer.setDocumentFromString(html);
+        renderer.layout();
+        renderer.createPDF(outputStream);
+
+        outputStream.close();
+    }
+
+    @GetMapping
+    public ResponseEntity<?> generatePDF() throws DocumentException, IOException {
+        String html = parseThymeleafTemplate();
+        generatePdfFromHtml(html);
+
+        return ResponseEntity.noContent().build();
+    }
 }
